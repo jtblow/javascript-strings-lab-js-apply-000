472b9f00ea83997a98146c5cc6b3d951cbd68a3f
creative-zombie-4458 <jtbartlow@gmail.com>
2019-01-22 22:14:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
-"Hello, " + specialGuest + "!"
+var greetSpecialGuest = "Hello" + specialGuest + "!";
+
 var topic = "space";
 
 var conversation = `${topic}`;
